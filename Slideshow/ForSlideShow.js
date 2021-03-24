2d9d3d9c0409f7b09868069bde6818476c386a8c
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-03-24 20:07:31 +0400

    dsfsdF

--- Slideshow/ForSlideShow.js
+++ Slideshow/ForSlideShow.js
@@ -1,3 +1,33 @@
+
+    var IMG = {
+        "IMGarray": [
+          "src/Animated_Wallpaper_Windows_10_-_Wallpaper_Engine.gif",
+          "src/0a4dcb92fa2d3c601b58d72720d6bec4.jpg",
+          "src/1579461169_preview_Recent Best.jpg"
+        ]
+      }
+
+    var obj = IMG.IMGarray[0];
+
+
+
+
+$( document ).ready(function() {
+    for (i = 0; i < 3; i++) {
+      $('.slideshow-container').append(
+    '<div class="mySlides fade">' +
+     ' <div class="numbertext">1 / 3</div> ' +
+      '<img src="src/Animated_Wallpaper_Windows_10_-_Wallpaper_Engine.gif" style="width:100%">' +
+     '<div class="text">Caption Text</div>' +
+     ' </div>'
+   );
+    
+    }
+    });
+
+
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
